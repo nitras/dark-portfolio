51b256ee3db2bf638932a4fc6c954729d789445c
Peter Eeckelaert <peter@nitras.be>
2019-09-08 17:23:37 +0200

    Update Hero.vue

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,7 +1,13 @@
 <template>
   <section class="hero">
-    <h1 class="line">{{data.header_title}}</h1>
-    <p class="line">{{data.header_paragraph}}</p>
+    <div class="option-A" v-if="Math.random() > 0.5">
+      <h1 class="line">{{data.header_title}}</h1>
+      <p class="line">{{data.header_paragraph}}</p>
+    </div>
+    <div class="option-B" v-else>
+      <h1 class="line">{{data.header_title}}</h1>
+      <p class="line">{{data.header_paragraph_alternative}}</p>
+    </div>
   </section>
 </template>
 
@@ -17,11 +23,16 @@ export default {
   },
   mounted() {
     TweenMax.staggerFrom(".line", 0.3, { y: 25, opacity: 0 }, 0.16);
+  },
+  methods: {
+    WaitAndChange: function() {}
   }
 };
 </script>
 
 <style lang="scss" scoped>
+@import "@/scss/nitras.scss";
+
 .hero {
   padding: 4rem 2rem 5rem 2rem;
   font-weight: 400;
@@ -29,10 +40,24 @@ export default {
   letter-spacing: 0.05em;
   h1 {
     font-size: 9vh;
+    @include breakpoint(xs) {
+      font-size: 8vh;
+    }
   }
   p {
     font-size: 2rem;
     max-width: 77%;
+    @include breakpoint(xs) {
+      max-width: 100%;
+    }
   }
+  /*@
+  .option-A {
+    opacity: 0;
+  }
+  .option-B {
+    opacity: 0;
+  }
+  */
 }
 </style>
